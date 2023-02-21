6a59cb66ba2e86afdea03502a8e6dd0f9b307262
Graziele Oliveira <devgrazioliveira@gmail.com>
2023-02-21 14:15:51 -0300

    add star rank to types

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,21 @@
 export interface ITraineeInfo {
-    name: string;
-    group: string;
-    ep1: number;
-    ep2: number;
-    ep3: number;
-    company: string;
-    dob: number;
-    id: number;
-    height: number;
-    hobby: string;
-    good_at: string;
-    phrase: string;
-    wb_supertopic?: string;
+  name: string;
+  group: string;
+  ep1: number;
+  ep2: number;
+  ep3: number;
+  company: string;
+  dob: number;
+  id: number;
+  height: number;
+  hobby: string;
+  good_at: string;
+  phrase: string;
+  wb_supertopic?: string;
+  star_rank1?: number;
+  star_rank2?: number;
 }
 
 export interface ITraineeInfoWithImage extends ITraineeInfo {
-    image: string;
+  image: string;
 }
