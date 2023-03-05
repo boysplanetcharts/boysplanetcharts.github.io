d1ca818ceae6e9ab1d1434d154afb541bd8a1a59
Graziele Oliveira <devgrazioliveira@gmail.com>
2023-03-05 12:36:16 -0300

    feat: add country flag

--- src/components/Footer/component.tsx
+++ src/components/Footer/component.tsx
@@ -1,27 +1,34 @@
 import './styles.css'
+import { BR } from 'country-flag-icons/react/3x2'
+
 
 export const Footer = () => {
   return (
     <div
       className="footer_div"
     >
-      <p style={{ fontWeight: "bold", fontSize: 10 }}>
+      <p style={{ fontWeight: "bold", fontSize: 9 }}>
         Inspired by:{" "}
         <a target="_blank" href="https://p101s2.github.io/">
           Produce 101 Season 2 Rankings
         </a>
       </p>
-      <p style={{ fontWeight: "bold", fontSize: 10 }}>
+      <p style={{ fontWeight: "bold", fontSize: 9 }}>
         Contribute to the project on{" "}
         <a target="_blank" href="https://github.com/boysplanetcharts/boysplanetcharts.github.io">
           Github
         </a>
       </p>
-      <p style={{ fontWeight: "bold", fontSize: 10, textAlign: "center" }}>
+      <p style={{ fontWeight: "bold", fontSize: 9, textAlign: "center" }}>
         If you find any errors or inaccuracies, you can send me a message on{" "}
         <a target="_blank" href="https://twitter.com/skawngrazi">
           Twitter
         </a>
+      </p>
+      <p style={{ fontWeight: "bold", fontSize: 9, textAlign: "center", paddingTop: 8 }}>
+
+        Made in <BR fontSize={8} style={{width: 12, paddingLeft: 2}} />
+
       </p>
     </div>
   );
